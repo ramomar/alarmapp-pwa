a177c6a975084e877056e18751ca4e895999a567
Eduardo Garza <gedu@edu-mbp.local>
2023-07-02 17:19:45 -0600

    Small test

--- lib/alarm_status/alarm_status_page.dart
+++ lib/alarm_status/alarm_status_page.dart
@@ -16,6 +16,7 @@ class AlarmStatusPage extends StatelessWidget {
           padding: const EdgeInsets.only(left: 28, right: 28, top: 32),
           child: Stack(
             children: [
+              const Text('v'),
               ...switch (state) {
                 MonitoredState(state: final s) => [
                     Center(
